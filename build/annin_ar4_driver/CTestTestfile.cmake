3dbb5ea38b269d0a555de0d6f3c01ed8d12fbb29
Jonas Thorhauge <jonas.thorhauge@gmail.com>
2025-05-24 15:30:54 +0200

    first commit

---
+++ build/annin_ar4_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/johnbuntu/ar4_ros_driver/annin_ar4_driver
+# Build directory: /home/johnbuntu/ar4_ros_driver/build/annin_ar4_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/annin_ar4_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/johnbuntu/ar4_ros_driver/install/annin_ar4_description/share/annin_ar4_description/package.sh"
+. "/home/johnbuntu/ar4_ros_driver/install/annin_ar4_driver/share/annin_ar4_driver/package.sh"
